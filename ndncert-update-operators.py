26807e3bb2e880758cc3e0cda6e621e415dc3006
Alexander Afanasyev <aa@cs.fiu.edu>
2018-04-04 11:28:45 -0400

    Add script to update operator dataset

---
+++ ndncert-update-operators.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+from pymongo import MongoClient
+import json
+import argparse
+
+parser = argparse.ArgumentParser(description='Update NDNCERT operators')
+parser.add_argument('file', metavar='file', type=str, nargs='+', help='''operators.json file''')
+args = parser.parse_args()
+
+client = MongoClient()
+db = client.ndncert
+
+data = json.load(open(args.file[0]))
+
+db.operators.remove()
+
+for key, item in data.items():
+    print(key)
+    item['key']=item['cert']
+    del item['cert']
+    if type(item['site_emails']) == str:
+        item['site_emails'] = [item['site_emails']]
+    db.operators.insert(item)
